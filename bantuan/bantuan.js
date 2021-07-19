72d039a52a4b6476aa880904d5648cb330d8357e
christyohanes <yohaneschristian2104@gmail.com>
2021-07-19 15:08:27 +0700

    add code

---
+++ bantuan/bantuan.js
@@ -0,0 +1,8 @@
+
+var klik = document.getElementById("li-1");
+var caraDaftar = document.getElementById("konten-faq")
+
+klik.addEventListener("click", function() {
+  klik.style.color = "#2F80ED"
+  caraDaftar.style.display = "block"
+});
---
+++ jualtiket/index.js
@@ -0,0 +1,17 @@
+
+const cardOnclickImg=document.querySelectorAll('.card img')
+const cardOnclick=document.querySelectorAll('.card')
+
+
+cardOnclickImg.forEach(element => {
+  console.log(element.getAttribute(`src`))
+  
+});
+
+cardOnclick.forEach(element => {
+  element.onclick=()=>{
+    location.href="tiket/tiket.html"
+  }
+});
+
+
